6439a98ae0eeb962319ef79600e0411d9d9fa1a7
okayu06 <magicsunset0805@gmail.com>
2019-03-06 18:16:52 +0900

    first

---
+++ memo.py
@@ -0,0 +1,16 @@
+d1 >> play("(x[--])xo{-[--][-x]}")
+print(SynthDefs)
+
+import numpy as np
+vec1 = np.array([1,2,3])
+print(vec1)
+
+a1 >> play("/Volumes/Lofi/art/sonicpi/sample/utada_jap/Utada Hikaru SINGLE COLLECTION VOL. 2 [Disc 1]/1-13-Beautiful-World-PLANiTb-Acoustica-Mix-.wav")
+
+
+a1 >> play("/Volumes/Lofi/art/sonicpi/sample/utada_jap/Utada Hikaru SINGLE COLLECTION VOL. 2 [Disc 1]/1-05-Beautiful-World.wav")
+
+
+a1 >> loop("/Volumes/Lofi/art/sonicpi/sample/utada_jap/First Love/04-First-Love.wav")
+
+p1 >> play("F", dur=[1])
